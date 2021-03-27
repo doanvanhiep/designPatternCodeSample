6b3f2a4e7ff5071dd0d6d21c6c4b550ced7f0b81
doanvanhiep <doanvanhiepebn951@gmail.com>
2021-03-28 00:36:38 +0700

    add singleton pattern

---
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonClass s1 = SingletonClass.GetInstance();
+            SingletonClass s2 = SingletonClass.GetInstance();
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ SingletonPattern/SingletonPattern/SingletonClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class SingletonClass
+    {
+        private static SingletonClass instance;
+        private SingletonClass()
+        {
+            Console.WriteLine("Created");
+        }
+        public static SingletonClass GetInstance()
+        {
+            Console.WriteLine("Called");
+            if (instance == null)
+            {
+                instance = new SingletonClass();
+            }
+
+            return instance;
+        }
+
+
+    }
+}
